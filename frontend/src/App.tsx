4f417d3947e3f36c853f6ff7f74201daf2c2447a
Marcus Dimitri <marcusdimitri7@gmail.com>
2021-09-07 19:46:07 -0300

    Implementado componente NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import NavBar from "componentes/NavBar";
 
 function App() {
   return (
     <div> 
 
 <h1 className="text-primary"> Olá Mundo!</h1>
+
+<NavBar></NavBar>
 
 
     </div>
---
+++ frontend/src/componentes/NavBar/index.tsx
@@ -0,0 +1,13 @@
+
+function NavBar() {
+    return (
+        <div>
+
+            <h2 className="text-primary"> Suparior</h2>
+
+
+        </div>
+    );
+}
+
+export default NavBar;
