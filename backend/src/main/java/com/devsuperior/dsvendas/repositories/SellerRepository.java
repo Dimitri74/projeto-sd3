5c4a10277897001eddb6f35037bbc1ebc7559cac
Marcus Dimitri <marcusdimitri7@gmail.com>
2021-09-09 19:31:13 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.devsuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+/**
+ * @author Marcus Dimitri
+ *
+ */
+public interface SellerRepository  extends JpaRepository<Seller, Long>{
+	
+	
+	
+	
+
+}
